5d658c5633bc36f28335a61f6432ba671e6e8e81
tc4dy <140116264+tc4dy@users.noreply.github.com>
2026-09-01 15:01:08 +0300

    Update main.go

--- cmd/godas/main.go
+++ cmd/godas/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
 	"time"
 )
 
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
+
 	cmd := NewRootCmd()
 	if err := cmd.ExecuteContext(ctx); err != nil {
-		log.Fatal(err)
+		log.Printf("Error: %v\n", err)
+		os.Exit(1)
 	}
 }
